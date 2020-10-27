234b32d72e9570b468657157b916575b0bb21efb
shin.ota <shin.ota0817@gmail.com>
2020-10-27 15:12:34 +0900

    Initial Commit

---
+++ table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  table
+//
+//  Created by Shin Ota on 10/27/20.
+//  Copyright © 2020 Shin Ota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
